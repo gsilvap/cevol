0e1bee7ae8b33cacaf0ad48b5d208b3fabf7b730
pedrocarmona <pcarmona1990@gmail.com>
2014-02-26 00:42:26 +0000

    hotfix by goncalo

--- pl2/joao_brandao/ernesto/joão_brandão_hc.py
+++ pl2/joao_brandao/ernesto/joão_brandão_hc.py
@@ -1,5 +1,5 @@
- """
-Números de João Brandão.
+"""
+Numeros de Joao Brandao.
 
 Algoritmo: Hill-climbing
 Pertubation: best neighbor
@@ -17,17 +17,17 @@ import random
 # Basic Hill Climbing
 
 def jb_hc(problem_size, max_iter,fitness):
-    candidate = random_indiv(problem_size,problem_size)
-    cost_candi = fitness(candidate)
+    candidate = random_indiv(problem_size)
+    cost_candi = fitness(candidate,problem_size)
     for i in range(max_iter):
         next_neighbor = best_neighbor(candidate,fitness)
         cost_next_neighbor = fitness(next_neighbor,problem_size)
-        if cost_next_neighbor >= cost_candi: 
+        if cost_next_neighbor >= cost_candi:
             candidate = next_neighbor
-            cost_candi = cost_next_neighbor        
+            cost_candi = cost_next_neighbor
     return candidate
- 
-    
+
+
 # Random Individual
 def random_indiv(size):
     return [random.randint(0,1) for i in range(size)]
@@ -54,25 +54,25 @@ def viola(indiv,comp):
     # count violations of constraints
     v = 0
     for elem in indiv:
-	    limite = min(elem-1,comp - elem)
-	    vi = 0
-	    for j in range(1,limite+1):
-		    if ((elem - j) in indiv) and ((elem+j) in indiv):
-			    vi += 1
-	    v += vi
+        limite = min(elem-1,comp - elem)
+        vi = 0
+        for j in range(1,limite+1):
+            if ((elem - j) in indiv) and ((elem+j) in indiv):
+                vi += 1
+        v += vi
     return v
-    
+
 # Auxiliar
 
 def fenotipo(indiv):
     fen = [i+1 for i in range(len(indiv)) if indiv[i] == 1]
     return fen
 
-    
+
 if __name__ == '__main__':
-	# For test purposes: beware of the time it may takes...
+    # For test purposes: beware of the time it may takes...
     res = fenotipo(jb_hc(100,10000,evaluate))
     quali = viola(res, len(res))
     print(quali,'\n', len(res), '\n',res)
-  
-    
+
+
