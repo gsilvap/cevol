2aa1085da164b4b383d7ac9ee6651e8713dbd3a2
pedrocarmona <pcarmona1990@gmail.com>
2014-05-06 19:25:05 +0100

    correction

--- projecto/README.md
+++ projecto/README.md
@@ -20,7 +20,7 @@ Para cada algoritmo faça 30 testes (runs). Recolha os dados sobre desem- penho 
 
 
 
-Problema de teste:
+##Problema de teste:
 
 8.6 Soma de Subconjuntos de Inteiros
 Este problema  é um caso particular do problema da mochila. Dado um conjunto de n inteiros distintos, a1, a2, . . . , an, não necessariamente consecutivos, encontrar um subconjunto desses inteiros, logo sem repetições, cuja soma  é igual a um dado X. Admita que pretende a solução de menor cardinalidade. Por exemplo, se tivermos o conjunto {5, 8, 4, 11, 6, 12} e X = 20 então temos as soluções {8, 12} e {4, 5, 11}, sendo a primeira a que nos interessa.
---
+++ projecto/src/pcarmona.py
@@ -0,0 +1,40 @@
+__author__ = 'PedroCarmona'
+
+# Algoritmo genetico
+
+def mutacao():
+    pass
+
+
+def recombinacao():
+    pass
+
+
+
+"""
+    Fazer 30 runs, medir o desempenho com:
+    - qualidade do algoritmo
+    - rapidez com que foi encontrado o melhor resultado
+
+    Analise estatistica dos resultados e tirar conclusões
+"""
+
+
+# varicao das probabilidades de mutacao e recombinação
+# mutação no inicio e recombinação no fim ?
+if __name__ == '__main__':
+    # 1º TESTE FIXO
+    # cruzamento = 0.9
+    # mutacao = 0.1
+
+    # 2º TESTE PROBABILIDADE INICIAL E FINAL
+    # cruzamento = 0.8 durante 70% das gerações, 0 nas seguintes
+    # mutacao = 0      durante 70% das gerações, 0.05 nas seguintes
+
+    # 3º TESTE PROBABILIDADES VARIAVEIS
+    # cruzamento  a descer de 0.9 a 0.7,  0.5, 0.3
+    # mutacao     a descer de 0.01, 0.05, 0.1, 0.2
+    # quatro pares de valores
+    # definir como é feita a variação
+
+    pass
